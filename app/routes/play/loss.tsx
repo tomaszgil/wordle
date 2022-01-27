30656f4f6c55badb662d07dfeb1b05fce3d5fbc4
Tomasz Gil <tomaszgil.dev@gmail.com>
2022-01-27 22:30:16 +0100

    Add words

--- app/routes/play/loss.tsx
+++ app/routes/play/loss.tsx
@@ -1,9 +1,25 @@
 import React from "react";
-import { redirect } from "remix";
+import { json, LoaderFunction, redirect, useLoaderData } from "remix";
 import type { ActionFunction } from "remix";
-import { getSession, destroySession } from "~/sessions";
+import { getSession, destroySession, commitSession } from "~/sessions";
 import { Dialog } from "~/components/Dialog";
 import { Button } from "~/components/Button";
+import { Mark } from "~/components/Mark";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const session = await getSession(request.headers.get("Cookie"));
+
+  return json(
+    {
+      word: session.get("word"),
+    },
+    {
+      headers: {
+        "Set-Cookie": await commitSession(session),
+      },
+    }
+  );
+};
 
 export const action: ActionFunction = async ({ request }) => {
   const session = await getSession(request.headers.get("Cookie"));
@@ -14,14 +30,17 @@ export const action: ActionFunction = async ({ request }) => {
 };
 
 export default function Loss() {
+  const { word } = useLoaderData<{ word: string }>();
+
   return (
     <Dialog>
       <div className="text-center">
         <div className="text-8xl mb-4">🥺</div>
         <h2 className="text-3xl mb-4 font-semibold">Ooops...</h2>
         <p className="max-w-lg mb-6">
-          That was hard. Typically you wouldn't be able to play again. Here you
-          can acutally try again!
+          That was hard - it was <Mark>{word}</Mark> you were looking for.
+          Typically you wouldn't be able to play again. Here you can acutally
+          try again!
         </p>
         <form method="post">
           <Button type="submit">Play again</Button>
